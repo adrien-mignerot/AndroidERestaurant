00822344e59eb25c01655f808d25a66d3a456803
Adrien MIGNEROT <adrienmignerot@yahoo.fr>
2021-01-25 17:33:36 +0100

    Fin jour 1

--- src/main/java/fr/isen/mignerot/androiderestaurant/CategoryActivity.kt
+++ src/main/java/fr/isen/mignerot/androiderestaurant/CategoryActivity.kt
@@ -3,6 +3,7 @@ package fr.isen.mignerot.androiderestaurant
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
 import fr.isen.mignerot.androiderestaurant.databinding.ActivityCategoryBinding
 import fr.isen.mignerot.androiderestaurant.databinding.ActivityHomeBinding
 
@@ -15,5 +16,8 @@ class CategoryActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.categoryTitle.text = intent.getStringExtra(HomeActivity.CATEGORY)
+
+        binding.listCategory.layoutManager = LinearLayoutManager(this)
+        binding.listCategory.adapter = CategoryListAdapter(resources.getStringArray(R.array.dish_name_array).toList())
     }
 }
---
+++ src/main/java/fr/isen/mignerot/androiderestaurant/CategoryListAdapter.kt
@@ -0,0 +1,27 @@
+package fr.isen.mignerot.androiderestaurant
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import fr.isen.mignerot.androiderestaurant.databinding.CategoryCellBinding
+
+class CategoryListAdapter(private val categories: List<String>): RecyclerView.Adapter<CategoryListAdapter.CategoryHolder>() {
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): CategoryHolder {
+        val itemBinding = CategoryCellBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return CategoryHolder(itemBinding)
+    }
+
+    override fun onBindViewHolder(holder: CategoryListAdapter.CategoryHolder, position: Int) {
+        holder.title.text = categories[position]
+    }
+
+    override fun getItemCount(): Int = categories.size
+
+    class CategoryHolder(binding: CategoryCellBinding) : RecyclerView.ViewHolder(binding.root) {
+        val title = binding.dishName
+    }
+}
