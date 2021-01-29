52bb841fb0f12d5ae69256e521ba08ddfb8c1c70
Adrien MIGNEROT <adrienmignerot@yahoo.fr>
2021-01-29 16:59:56 +0100

    Partie 4 : Question C

--- src/main/java/fr/isen/mignerot/androiderestaurant/DetailActivity.kt
+++ src/main/java/fr/isen/mignerot/androiderestaurant/DetailActivity.kt
@@ -1,7 +1,10 @@
 package fr.isen.mignerot.androiderestaurant
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import com.squareup.picasso.Picasso
 import fr.isen.mignerot.androiderestaurant.databinding.ActivityDetailBinding
 import fr.isen.mignerot.androiderestaurant.model.Dish
@@ -9,6 +12,7 @@ import fr.isen.mignerot.androiderestaurant.model.Dish
 private lateinit var binding: ActivityDetailBinding
 
 class DetailActivity : AppCompatActivity() {
+    private var compteur = 1
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityDetailBinding.inflate(layoutInflater)
@@ -18,7 +22,11 @@ class DetailActivity : AppCompatActivity() {
         binding.detailTitle.text = dish.title
         binding.detailIngredients.text = dish.ingredients.joinToString(", ") { it.name }
 
-        binding.detailButton.text = "Total ".toUpperCase() + dish.getFormattedPrice()
+        binding.detailRemove.visibility = View.GONE
+
+        val total = dish.getPrice().toFloat()*compteur
+
+        binding.detailButton.text = "Total ".toUpperCase() + total.toString() + " €"
 
         dish.getAllPictures()?.let {
             binding.detailViewPager.adapter = DetailViewAdapter(this, it)
@@ -35,6 +43,33 @@ class DetailActivity : AppCompatActivity() {
                         .load(R.drawable.not_found)
                         .into(binding.detailPicture)
             }
+        }
+
+        binding.detailAdd.setOnClickListener {
+            if(compteur < 10){
+                compteur++
+                if(compteur > 1)
+                    binding.detailRemove.visibility = View.VISIBLE
+                binding.detailNumber.text = compteur.toString()
+            } else {
+                Toast.makeText(applicationContext, "Vous pouvez commander au maximum 10 ${dish.title}.", Toast.LENGTH_LONG).show()
+                binding.detailAdd.visibility = View.GONE
+            }
+            val total = dish.getPrice().toFloat()*compteur
+            binding.detailButton.text = "Total ".toUpperCase() + total.toString() + " €"
+        }
+
+        binding.detailRemove.setOnClickListener {
+            if(compteur > 1){
+                compteur--
+                if(compteur == 1)
+                    binding.detailRemove.visibility = View.GONE
+                if(compteur < 10)
+                    binding.detailAdd.visibility = View.VISIBLE
+                binding.detailNumber.text = compteur.toString()
+            }
+            val total = dish.getPrice().toFloat()*compteur
+            binding.detailButton.text = "Total ".toUpperCase() + total.toString() + " €"
         }
     }
 }
